a4cded33952dbf47fa0521e75e0601a92e6f05ba
Gleison-dev <gleisonrgomes@gmail.com>
2024-09-25 19:03:53 -0300

    feat: user entity created

---
+++ src/entities/user.entity.js
@@ -0,0 +1,37 @@
+import { Sequelize, DataTypes } from "sequelize";
+import { sequelize as database } from "../database/connection.js";
+
+const UserEntity = database.define("tb_user", {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4,
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  agency: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  acount: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  balance: {
+    type: DataTypes.DECIMAL,
+    defaultValue: 0,
+  },
+});
+
+export { UserEntity }
