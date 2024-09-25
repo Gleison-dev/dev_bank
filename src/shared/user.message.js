052c19160de6bc685603653946a835a524b535a5
Gleison-dev <gleisonrgomes@gmail.com>
2024-09-25 19:11:31 -0300

    feat: message of user created

---
+++ src/shared/user.message.js
@@ -0,0 +1,12 @@
+const ERRORS = {
+  ALREADY_EXISTS: "Usuário já existe. Tente novamente!",
+  NOT_FOUND: "Usuário não encontrado. Tente novamente!",
+  CPF_PASSWORD_INCORRET: "CPF e/ou senha incorretas. Tente novamente!"
+};
+
+const SUCESS = {
+    CREATED: "Usuário cadastrado com sucesso!",
+    DELETED: "Usuário deletado com sucesso!",
+    WITHDRAWAL: "Saque realizado com sucesso!",
+    TRANSFER: "Transferência realizada com sucesso!"
+}
