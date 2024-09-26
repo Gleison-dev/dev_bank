fc683a7148a9a51dbbd2900a512778841ac0cb49
Gleison-dev <gleisonrgomes@gmail.com>
2024-09-26 18:31:48 -0300

    feat: addind depoist message

--- src/shared/user.message.js
+++ src/shared/user.message.js
@@ -8,7 +8,8 @@ const SUCESS = {
     CREATED: "Usuário cadastrado com sucesso!",
     DELETED: "Usuário deletado com sucesso!",
     WITHDRAWAL: "Saque realizado com sucesso!",
-    TRANSFER: "Transferência realizada com sucesso!"
+    TRANSFER: "Transferência realizada com sucesso!",
+    DEPOSIT: "Depósito realizado com sucesso!"
 }
 
 export { ERRORS, SUCESS }
