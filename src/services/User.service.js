9a9f47bfdda14bd2a493301bb67c7404c3cd1ce8
Gleison-dev <gleisonrgomes@gmail.com>
2024-09-26 18:31:02 -0300

    feat: functions of user service created

---
+++ src/services/User.service.js
@@ -0,0 +1,85 @@
+import { UserEntity } from "../entities/user.entity.js";
+import { ERRORS, SUCESS } from "../shared/user.message.js";
+
+class UserService {
+  async createUserService(
+    first_name,
+    last_name,
+    cpf,
+    agency,
+    account,
+    password,
+    balance
+  ) {
+    try {
+      await UserEntity.sync();
+      const verifyUserExists = await UserEntity.findOne({
+        where: {
+          cpf,
+          password,
+        },
+      });
+      if (verifyUserExists) {
+        return `${ERRORS.ALREADY_EXISTS}`;
+      }
+      const newUser = await UserEntity.create({
+        first_name,
+        last_name,
+        cpf,
+        agency,
+        account,
+        password,
+        balance,
+      });
+      return `${SUCESS.CREATED}`;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getAllUsersService() {
+    try {
+      const allUsers = await UserEntity.findAll();
+      return allUsers;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getUserByCpfService(cpf) {
+    try {
+      const userByCpf = await UserEntity.findOne({
+        where: {
+          cpf,
+        },
+      });
+      if (!userByCpf) {
+        return `${ERRORS.NOT_FOUND}`;
+      }
+      return userByCpf;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async depositService(cpf, password, balance, deposit) {
+    try {
+      const verifyCpfAndPassword = await UserEntity.findOne({
+        where: {
+          cpf,
+          password,
+        },
+      });
+      if (!verifyCpfAndPassword) {
+        return `${ERRORS.CPF_PASSWORD_INCORRET}`;
+      }
+      const updateBalance = await UserEntity.update({ balance: deposit }, {
+        where: {
+            cpf
+        }
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+}
