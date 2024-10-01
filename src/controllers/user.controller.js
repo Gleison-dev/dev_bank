b93cd75fd8d754f535b5f74c628147759fac276c
Gleison-dev <gleisonrgomes@gmail.com>
2024-10-01 19:17:19 -0300

    feat: added more functions on controller and exporting

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { UserService } from "../services/User.service.js";
 
 const userServiceInstance = new UserService();
 
-const newUser = async (req, res) => {
+const createUser = async (req, res) => {
   const { first_name, last_name, cpf, agency, account, password, balance } =
     req.body;
   const newUser = await userServiceInstance.createUserService(
@@ -21,3 +21,29 @@ const getAllUsers = async (req, res) => {
   const allUsers = await userServiceInstance.getAllUsersService();
   res.json({ allUsers });
 };
+
+const getUserByCpf = async (req, res) => {
+  const { cpf } = req.params;
+  const userByCpf = await userServiceInstance.getUserByCpfService(cpf);
+  res.status({ userByCpf });
+};
+
+const depositService = async (req, res) => {
+  const { cpf } = req.params;
+  const { password, deposit } = req.body;
+  const serviceDeposit = await userServiceInstance.depositService(
+    cpf,
+    password,
+    deposit
+  );
+  res.json({ serviceDeposit });
+};
+
+const deleteUser = async (req, res) => {
+  const { cpf } = req.params;
+  const { password } = req.body;
+  const delUser = await userServiceInstance.deleteUser(cpf, password);
+  res.json({ delUser });
+};
+
+export { createUser, getAllUsers, getUserByCpf, depositService, deleteUser }
